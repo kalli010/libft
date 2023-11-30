338f8a0f0b55de5353ae32511d57bf43754faed4
kalli010 <elkalaizakariae010@gmail.com>
2023-11-30 11:32:14 +0100

    main

--- main.c
+++ main.c
@@ -1,17 +1,40 @@
 #include "libft.h"
-#include <fcntl.h>
 
-void tpr(unsigned int i, char *c)
+void ft_del(void *content)
 {
-	while(c[i] != '\0')
-	{
-	     	if (c[i] >= 'a' && c[i] <= 'z')
-        		c[i] = c[i] - 32;
-		i++;
-	}
+	free(content);
+}
+
+void *ft_a(void *f)
+{
+	printf("%s\n",(char *)f);
+}
+
+void ft_j(void *y)
+{
+	char *d = (char *)y;
+	d = d + 1;
 }
 
 int main ()
 {
+	t_list *b = ft_lstnew(ft_strdup(" 1 "));
+	t_list *f = ft_lstnew(ft_strdup(" 2 "));
+	t_list *c = ft_lstnew(ft_strdup(" 3 "));
+	t_list *head = NULL;
+
+	ft_lstadd_front(&head, c);
+	ft_lstadd_back(&head, b);
+	ft_lstadd_front(0, 0);
+	return 0;	
+	int s = ft_lstsize(head);
+	printf("%d\n",s);
+
+	ft_lstdelone(c,ft_del);
+	f->next = b;
 	
+	ft_lstiter(b, ft_j);
+	t_list *p = ft_lstmap(head, ft_a,ft_del);
+
+	free(p);
 }
